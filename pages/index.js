205a389e8ceb524f7f11244ae639a6b0a5cd11a7
kelvin peter <pkelvin856@gmail.com>
2023-05-30 23:22:53 -0700

    updated

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,16 @@ import Hero from '@/components/Hero'
 import Joincontainer from '@/components/Joincontainer'
 import Listyoga from '@/components/Listyoga'
 import Rountinecontainer from '@/components/Rountinecontainer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
    <div>
+     <Head>
+        <title>Yoga Arena</title>
+        <meta name="description" content="Yoga Arena" />
+        <link rel="icon" href="/daylight.png" />
+      </Head>
     <Hero/>
     <Listyoga/>
     <Healthcontainer/>
