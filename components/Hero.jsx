bae58fde365ef722b00b967a550860c05196bb2a
kelvin peter <pkelvin856@gmail.com>
2023-06-21 14:36:51 +0100

    saved

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,20 +16,20 @@ export default function Hero() {
   return (
     <div className=' text-center '> 
       <div className=' flex justify-center items-center '>
-       <div className=' mt-[5rem] text-[#220953]  relative left-12 '>
-       <div className=' text-4xl font-medium xl:mr-[37rem] xl:text-5xl '> Choose Your </div> 
-       <h1 className=' text-4xl font-medium mt-8 mr-[4.6rem] xl:mr-[48rem] xl:text-5xl '>Best</h1>
-       <h1 className=' text-4xl font-medium mt-4 ml-5  xl:mr-[45rem] xl:text-5xl '>Teacher</h1>
+       <div className=' mt-[5rem] text-[#220953]  relative left-12 md:-left-[8rem] md:top-11 lg:mr-[35rem] '>
+       <div className=' text-4xl font-medium '> Choose Your </div> 
+       <h1 className=' text-4xl font-medium mt-8 mr-[4.6rem] md:mr-[7rem] '>Best</h1>
+       <h1 className=' text-4xl font-medium mt-4 ml-5  md:mr-[5rem] '>Teacher</h1>
        </div>
-         <div style={gradientStyle} className=' xl:right-[46rem]  relative mt-[6rem] rotate-6 font-medium  px-4 py-3 rounded-lg  right-[3rem] text-5xl '>Yoga</div>
+         <div style={gradientStyle} className=' relative mt-[6rem] rotate-6 font-medium  px-4 py-3 rounded-lg  right-[3rem] text-5xl md:right-[15rem] md:top-10 lg:right-[50.5rem] '>Yoga</div>
       </div>
-      <div className='homestar mt-3 relative xl:-top-10 ml-2 xl:ml-[33rem]'>
+      <div className='homestar mt-3 relative md:ml-[20rem] lg:ml-[29rem] '>
         <div> <Image className='ml-[17rem] relative right-[13.5rem] bottom-[3rem] ' src={homestar1} alt='daylight' height={80} width={30}/></div>
         <div> <Image className='  ml-[17rem] relative right-[14rem] bottom-[4.3rem] ' src={homestar2} alt='daylight' height={80} width={45}/></div> 
         <div> <Image className='  ml-[17rem] relative right-[13rem] bottom-[6.2rem] ' src={homestar3} alt='daylight' height={80} width={15}/></div>
        </div>
-       <p className=' p-2 text-xl relative bottom-[5rem]  items-center xl:text-lg xl:w-[34%] xl:bottom-[4rem] xl:left-[12rem] '>Calm your mind and body with the best yoga teachers available, relax your day and stay positive.</p>
-       <div className='clippath relative xl:bottom-[7rem] xl:right-[5.7rem]'>
+       <p className=' p-2 text-xl relative bottom-[5rem]  items-center md:w-[40%] md:ml-16 md:-top-10 lg:w-[32%] lg:ml-[7rem] '>Calm your mind and body with the best yoga teachers available, relax your day and stay positive.</p>
+       <div className='clippath relative md:ml-[-12.7rem] md:bottom-20 '>
          <div>
          <Image className='  ml-[17rem] relative  bottom-[5rem] ' src={clippath1} alt='daylight' height={80} width={20}/>
          </div>
@@ -37,20 +37,20 @@ export default function Hero() {
 
        </div>
        <div>
-       <button style={gradientStyle} className=' text-[#4B3E65] relative bottom-16 xl:bottom-[6rem] xl:right-[20rem]  py-5 px-16 rounded-[34px] text-2xl'>Get Started Free</button>
+       <button style={gradientStyle} className=' text-[#4B3E65] relative bottom-16   py-5 px-16 rounded-[34px] text-2xl md:text-xl md:right-[12rem]   md:py-5 md:px-12 lg:mr-[35rem]   '>Get Started Free</button>
        <div>
-       <Image className=' ml-[18rem] relative  bottom-[6.6rem] xl:-top-[8.7rem] xl:ml-[28rem] ' src={arrowrightline} alt='daylight' height={80} width={20}/> 
+       <Image className=' ml-[18rem] relative  bottom-[6.6rem] md:ml-[18.8rem] lg:ml-[22.8  rem] ' src={arrowrightline} alt='daylight' height={80} width={20}/> 
        </div>
        </div>
        <div className=' items-center '>
-       <div className=' relative ml-[12rem] bottom-[4rem] text-[#220953] xl:right-[17rem]  '> <h1 className=' text-4xl xl:text-6xl'>40+</h1>
+       <div className=' relative ml-[12rem] bottom-[4rem] text-[#220953] md:-ml-[12rem]  '> <h1 className=' relative text-4xl '>40+</h1>
         <p className=' text-lg text-[#220953]'>Participants</p></div>
 
-        <div className=' relative bottom-[7.9rem] mr-[10rem] text-[#220953] xl:mt-[5rem] xl:right-[20rem] xl:bottom-[13.9rem]'><h1 className=' text-4xl xl:text-6xl'>300+</h1>
+        <div className=' relative bottom-[7.9rem] md:ml-[-22rem] mr-[10rem] text-[#220953]'><h1 className=' text-4xl  '>300+</h1>
         <p className=' text-lg text-[#220953] '>Yoga Class</p></div>
        </div>
        
-       <Image className=' ml-[3rem] bottom-20 xl:bottom-20 xl:mt-[-40rem] relative items-center  xl:left-[38rem] xl:w-[34%]  ' src={homeyoga} alt='daylight' height={80} width={313}/> 
+       <Image className=' ml-[3rem] bottom-20  relative items-center md:bottom-[39rem] md:left-[24rem] lg:ml-[18rem] lg:w-[30%]  ' src={homeyoga} alt='daylight' height={80} width={313}/> 
        
     </div>
   )
